5c15c89df3b12dd1db7ef008c5572548362275ef
Mateus Dos Anjos <mateusdosanjos04@gmail.com>
2025-10-14 19:46:10 -0300

    condição

---
+++ aula11/ex10.js
@@ -0,0 +1,8 @@
+var país = 'França'
+console.log(`Estou vivendo no ${país}`)
+    if (país=='Brasil'){
+        console.log('Você é brasileiro!')
+    } 
+    else {
+        console.log('Você é estrangeiro!')
+    }

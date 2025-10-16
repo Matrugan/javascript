8651bc6d5fc26093e71052ec47eb5ce6f6e6f8ad
Mateus Dos Anjos <mateusdosanjos04@gmail.com>
2025-10-16 13:05:59 -0300

    jv

---
+++ aula17/ambiente.js
@@ -0,0 +1,14 @@
+var num = [1 , 5 , 9]
+/*
+num[3] = 6
+num.push(7)
+num.length
+num.sort()
+num.indexOf()
+console.log(`Nosso vetor é ${num.length}`)
+*/
+num.push(4)
+num.push(10)
+console.log(`Nosso vetor é ${num}`)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`Nosso primeiro vetor é ${num[0]}`)
